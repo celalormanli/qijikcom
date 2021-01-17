5cb8eecb2e1927f5505729089b8facfc7bd2bef0
Celal ORMANLI <ormanlicelal@gmail.com>
2021-01-17 17:28:30 +0300

    create models

--- word_memorization/admin.py
+++ word_memorization/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from word_memorization.models import Category
+from word_memorization.models import Word
+
+admin.site.register(Category)
+admin.site.register(Word)
--- word_memorization/models.py
+++ word_memorization/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name=models.CharField(max_length=100, blank=False, default='')
+
+
+
+class Word(models.Model):
+    english=models.CharField(max_length=100, blank=False, default=100)
+    turkish=models.CharField(max_length=100, blank=False, default=100)
+    german=models.CharField(max_length=100, blank=False, default=100)
+    french=models.CharField(max_length=100, blank=False, default=100)
+    category_name=models.ForeignKey(Category, on_delete=models.CASCADE)
