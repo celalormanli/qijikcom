1ed19a2422bd11bf2db33b42f6324450cb7c1223
Celal ORMANLI <ormanlicelal@gmail.com>
2021-01-25 00:49:59 +0300

    add checker for os system

--- qijikcom/settings.py
+++ qijikcom/settings.py
@@ -13,8 +13,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import sys
 
-load_dotenv(dotenv_path='.env')
+if sys.platform=='linux':
+    load_dotenv(dotenv_path='env')
+else:
+    load_dotenv(dotenv_path='.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
