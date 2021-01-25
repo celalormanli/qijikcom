12e1d9081d96b1fa9fb28d5a702616c21aabc2e5
Celal ORMANLI <ormanlicelal@gmail.com>
2021-01-25 06:45:36 +0300

    add redis for caching

--- word_memorization/views.py
+++ word_memorization/views.py
@@ -5,11 +5,21 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
 from word_memorization.models import Word, Category
+from django.core.cache import cache
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+
+CACHE_TTL=getattr(settings,'CACHE_TTL',DEFAULT_TIMEOUT)
 
 @api_view(['GET'])
 def word_list(request):
     if request.method=='GET':
-        words =  Word.objects.all()
+        words=None
+        if 'words' in cache:
+            words =  cache.get('words')    
+        else:
+            words =  Word.objects.all()
+            cache.set('words', words, timeout=CACHE_TTL)
         serializer = WordSerializer(words, many=True)
         return Response(serializer.data)
 
@@ -25,6 +35,11 @@ def word_list_filtered(request, fk):
 @api_view(['GET'])
 def category_list(request):
     if request.method=='GET':
-        categories=Category.objects.all()
+        categories=None
+        if 'categories' in cache:
+            categories=cache.get('categories')
+        else:
+            categories=Category.objects.all()
+            cache.set('categories',categories, timeout=CACHE_TTL)
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
